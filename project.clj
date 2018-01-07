df62dd41a39a5f364192a57a51fb997b355541f9
Ranjeet Singh <ranjeet@raeoks.com>
2018-01-07 14:10:55 +0530

    add nrepl host and port

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :local-repo "vendor"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.newrelic.agent.java/newrelic-api "3.45.0"]]
-  :profiles { :dev { :dependencies [[ring/ring-core "1.6.3"]
-                                    [ring-mock "0.1.5"]
-                                    [org.clojure/tools.trace "0.7.9"]]}}
+  :repl-options {:host "0.0.0.0" :port 1337}
+  :profiles {:dev {:dependencies [[ring/ring-core "1.6.3"]
+                                  [ring-mock "0.1.5"]
+                                  [org.clojure/tools.trace "0.7.9"]]}}
   :plugins [[lein-ancient "0.6.15"]])
